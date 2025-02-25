33e6152858d14b4639c1133158352cc8440fc1f4
Sophie <sophie.sykes@slwa.wa.gov.au>
2025-02-25 03:59:56 +0000

    new feature

---
+++ src/tumbleweed-node/install.sh
@@ -0,0 +1,6 @@
+# TODO: Handle if there is a remote user $_REMOTE_USER_HOME
+
+# Install the node package manager
+zypper in -y npm
+# And then upgrade to the latest version
+npm install -g npm@latest
