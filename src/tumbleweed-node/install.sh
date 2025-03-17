109d87176443d7b2c54ee34042c72ff88cd9629b
Sophie <70588905+probablySophie@users.noreply.github.com>
2025-03-17 15:24:15 +0800

    Update install.sh

--- src/tumbleweed-node/install.sh
+++ src/tumbleweed-node/install.sh
@@ -1,6 +1,10 @@
 # TODO: Handle if there is a remote user $_REMOTE_USER_HOME
 
-# Install the node package manager
-zypper in -y npm
-# And then upgrade to the latest version
-npm install -g npm@latest
+NODE_VERSION=${NODE_VERSION};
+
+# Install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/master/install.sh | bash
+echo node > .nvmrc
+
+nvm install ${NODE_VERSION};
+nvm use ${NODE_VERSION};
